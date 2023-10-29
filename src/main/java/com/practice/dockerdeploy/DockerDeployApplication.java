d7dc159e95c4b81b5aca835976e4447b13944e25
seate <86824703+seate@users.noreply.github.com>
2023-10-30 02:53:12 +0900

    FEAT: initial commit

---
+++ src/main/java/com/practice/dockerdeploy/DockerDeployApplication.java
@@ -0,0 +1,13 @@
+package com.practice.dockerdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDeployApplication.class, args);
+	}
+
+}
